707f6135f49559fc01674273f0b95139b881bb10
bilinskask <bilinskask@gmail.com>
2020-03-09 18:53:06 +0200

    first push

---
+++ src/pages/ProductsPage/ProductsPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Product from '../../components/Product'
+
+export default class ProductsPage extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    const fetchProducts = async () => {
+      const response = await axios.get('http://localhost:4000/products')
+      const products = response.data
+      this.setState({ products })
+    }
+    fetchProducts()
+  }
+
+  render() {
+    const { products } = this.state
+    return (
+      <div className='container'>
+        {products.map(product => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    )
+  }
+}
