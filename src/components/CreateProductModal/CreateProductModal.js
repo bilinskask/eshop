21ac4f217452be332bb71bae9a41585cb393836e
bilinskask <bilinskask@gmail.com>
2020-03-12 21:36:32 +0200

    More stuff

--- src/components/CreateProductModal/CreateProductModal.js
+++ src/components/CreateProductModal/CreateProductModal.js
@@ -4,6 +4,10 @@ import classes from './CreateProductModal.module.css'
 import { random } from 'faker'
 import axios from 'axios'
 
+//qunatity is string
+//close modal on cancel
+//show success message and clear input fields
+
 export const CreateProductModal = ({ isModalOpen, toggleModal }) => {
   const [formState, setFormState] = useState({
     id: random.uuid(),
@@ -11,28 +15,50 @@ export const CreateProductModal = ({ isModalOpen, toggleModal }) => {
     description: '',
     price: 0,
     quantity: 0,
-    image: ''
+    image: `https://picsum.photos/id/${random.number(200) || 1}/600`
   })
+  const [errors, setErrors] = useState({})
+  const [success, setSuccess] = useState(false)
 
   const generateDescriptionHandler = event => {
-    if (validate()) {
-      event.preventDefault()
-      setFormState({
-        ...formState,
-        description: random.words(250)
-      })
-    } else {
-      alert('Form is invalid!')
-    }
+    event.preventDefault()
+    setFormState({
+      ...formState,
+      description: random.words(250)
+    })
   }
 
   const validate = () => {
     let isFormValid = true
+    const errors = {}
     if (!formState.name || formState.name.length < 5) {
       isFormValid = false
+      errors.name = 'Name must be at least 5 characters'
     }
+    if (!formState.quantity || formState.quantity <= 0) {
+      isFormValid = false
+      errors.quantity = 'Quantity is required'
+    }
+    if (!formState.price) {
+      isFormValid = false
+      errors.price = 'Price is required'
+    }
+    console.log(errors)
+    setErrors(errors)
     return isFormValid
   }
+
+  const quantityChangeHandler = event => {
+    const numberQuantity = Number(event.target.value)
+    const quantity = numberQuantity <= 0 ? 0 : numberQuantity
+    setFormState({ ...formState, quantity })
+  }
+
+  // const priceChangeHandler = event => {
+  //   const priceNumber = Number(event.target.value)
+  //   const price = priceNumber.toFixed(2)
+  //   setFormState({ ...formState, price })
+  // }
 
   const inputChangeHandler = (formStateKey, event) => {
     setFormState({
@@ -43,9 +69,27 @@ export const CreateProductModal = ({ isModalOpen, toggleModal }) => {
 
   const formSubmitHandler = async event => {
     event.preventDefault()
-    const response = await axios.post('http://localhost:4000/products', {
-      ...formState
+    if (validate()) {
+      const formStateCopy = {
+        ...formState,
+        price: Number(formState.price).toFixed(2)
+      }
+      const response = await axios.post('http://localhost:4000/products', {
+        ...formStateCopy
+      })
+      response.status === 201 && setSuccess(true)
+    }
+  }
+
+  const resetForm = () => {
+    setFormState({
+      ...formState,
+      name: '',
+      description: '',
+      price: 0,
+      quantity: ''
     })
+    setSuccess(false)
   }
 
   return (
@@ -56,51 +100,73 @@ export const CreateProductModal = ({ isModalOpen, toggleModal }) => {
     >
       <div>
         <h2 className={classes.heading}>Create product</h2>
-        <form onSubmit={formSubmitHandler}>
-          <div className={classes.formControl}>
-            <label htmlFor='name'>Product name</label>
-            <input
-              type='text'
-              id='name'
-              value={formState.name}
-              onInput={event => inputChangeHandler('name', event)}
-            />
-          </div>
-          <div className={classes.formControl}>
-            <label htmlFor='description'>Description</label>
-            <textarea
-              id='description'
-              rows='4'
-              value={formState.description}
-              onInput={event => inputChangeHandler('description', event)}
-            ></textarea>
-            <button onClick={generateDescriptionHandler}>
-              Generate Description
-            </button>
-          </div>
-          <div className={classes.formControl}>
-            <label htmlFor='price'>Price</label>
-            <input
-              type='number'
-              id='price'
-              value={formState.price}
-              onInput={event => inputChangeHandler('price', event)}
-            />
-          </div>
-          <div className={classes.formControl}>
-            <label htmlFor='quantity'>Quantity</label>
-            <input
-              type='number'
-              id='quantity'
-              value={formState.quantity}
-              onInput={event => inputChangeHandler('quantity', event)}
-            />
-          </div>
-          <div className={classes.buttonList}>
-            <button>Cancer</button>
-            <button>Create</button>
-          </div>
-        </form>
+        {success ? (
+          <>
+            <h2>Product Created</h2>
+            <div className={classes.buttonList}>
+              <button onClick={toggleModal}>Cancer</button>
+              <button onClick={resetForm}>Create</button>
+            </div>
+          </>
+        ) : (
+          <form onSubmit={formSubmitHandler}>
+            <div className={classes.formControl}>
+              <label htmlFor='name'>Product name</label>
+              <input
+                type='text'
+                id='name'
+                value={formState.name}
+                onInput={event => inputChangeHandler('name', event)}
+              />
+              {errors.name && (
+                <div className={classes.error}>{errors.name}</div>
+              )}
+            </div>
+            <div className={classes.formControl}>
+              <label htmlFor='description'>Description</label>
+              <textarea
+                id='description'
+                rows='4'
+                value={formState.description}
+                onInput={event => inputChangeHandler('description', event)}
+              ></textarea>
+              {errors.description && (
+                <div className={classes.error}>{errors.description}</div>
+              )}
+              <button onClick={generateDescriptionHandler}>
+                Generate Description
+              </button>
+            </div>
+            <div className={classes.formControl}>
+              <label htmlFor='price'>Price</label>
+              <input
+                type='number'
+                id='price'
+                value={formState.price}
+                onInput={event => inputChangeHandler('price', event)}
+              />
+              {errors.price && (
+                <div className={classes.error}>{errors.price}</div>
+              )}
+            </div>
+            <div className={classes.formControl}>
+              <label htmlFor='quantity'>Quantity</label>
+              <input
+                type='number'
+                id='quantity'
+                value={formState.quantity}
+                onInput={quantityChangeHandler}
+              />
+              {errors.quantity && (
+                <div className={classes.error}>{errors.quantity}</div>
+              )}
+            </div>
+            <div className={classes.buttonList}>
+              <button onClick={toggleModal}>Cancer</button>
+              <button>Create</button>
+            </div>
+          </form>
+        )}
       </div>
     </Modal>
   )
